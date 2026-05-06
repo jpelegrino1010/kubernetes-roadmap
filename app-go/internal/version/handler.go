b30c3da1da95e4c05579bb27a41be458256c8213
Julio Luis <jpelegrino@intercore.tech>
2026-05-06 16:36:38 -0400

    Add env variable

--- app-go/internal/version/handler.go
+++ app-go/internal/version/handler.go
@@ -3,11 +3,18 @@ package version
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+const defaultVersion = "version 2"
 
 func Handler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		v := os.Getenv("APP_VERSION")
+		if v == "" {
+			v = defaultVersion
+		}
 		w.Header().Set("Content-Type", "text/plain")
-		fmt.Fprint(w, "version 2")
+		fmt.Fprint(w, v)
 	}
 }
